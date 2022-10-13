582a3ab026657b7c309be9b267e04f1b2a4ff0f8
Mikel Laorden <laordenmikel@gmail.com>
2022-10-13 11:24:35 +0200

    los avances

--- src/erlojua/erlojua.c
+++ src/erlojua/erlojua.c
@@ -1,51 +1,77 @@
 #include <pthread.h>
 #include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#define TIMER_KOP   2;
+#define TIMER_KOP   1;
 
 void *timer_start();
+void timer_send_signal();
+void *clock_start(void *vargp);
 
 pthread_mutex_t mutex_clk = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond_clk_ziklo = PTHREAD_COND_INITIALIZER;
 pthread_cond_t cond_tmr_arreta = PTHREAD_COND_INITIALIZER;
 
-int timer_ticks = 15;
+int main()
+{
+    printf("start\n");
+    
+    pthread_t tr_clk, tr_tmr1, tr_tmr2;
 
-int main(){
+    int timer_ticks = 3000;
+    int timer_kop = TIMER_KOP;
+    printf("Clock started\n");
+    if (pthread_create(&tr_clk, NULL, clock_start, (void *) &timer_kop)){
+        printf("Error: Clock started\n");
+    }
+    
+    printf("Timer started\n");
+    if (pthread_create(&tr_tmr1, NULL, timer_start, (void *) &timer_ticks)){
+        printf("Error: Timer started"); 
+    }
+    
+    pthread_join(tr_clk, NULL);
+    //pthread_join(tr_tmr1, NULL); 
+    
+    exit(0);
+}
 
-    pthread_t tr_tmr1, tr_tmr2;
 
+void *clock_start(void *vargp){
+    printf("Hey im clock");
+    int *p_timer_kop = (int *)vargp;
     int done;
     while(1){
         done = 0;
-        pthread_mutex_lock(&mutex_clk);
-        while ( done < TIMER_KOP )
+        pthread_mutex_lock(&mutex_clk); // Mutexa blokeatu
+        while ( done < *p_timer_kop )
         {
             pthread_cond_wait(&cond_tmr_arreta, &mutex_clk );
             done++;
-
         }
-        
+        pthread_cond_broadcast(&cond_clk_ziklo);
     }
 }
 
-void *timer_start(){
-    int count = timer_ticks;
-    pthread_mutex_lock(&mutex_clk);
+void *timer_start(void *argv){
+    printf("Timer started");
+    int *count = (int *) argv;
+    pthread_mutex_lock(&mutex_clk); // zer gertatuko litzateke, lerro hau clock-aren mutex-lock-a baino lehen exekutatuko balitz?
     while(1){
         count--;
-
         if (count < 0){
-            kill()
+            timer_send_signal();
+            count=(int *) argv;
         }
 
+        pthread_cond_signal(&cond_tmr_arreta);
         pthread_cond_wait(&cond_clk_ziklo, &mutex_clk);
-
-
-
     }
 }
 
 void timer_send_signal(){
-
+    printf("Signal sent");
 }
+
+
