3cde9dd3008acbb656c30d66eab8d6386f88332c
Mikel Laorden <laordenmikel@gmail.com>
2022-10-24 19:16:31 +0200

    Prints para comprobar flujo. Intentar que no se bloque
    Ahora , el flujo del programa ha sido cambiado para evitar deadlocks.
    
    Ahora se evita el deadlock que surgia por el envio prematuro de la señal
    cond_timer_arreta del TIMER al CLK.
    
    Se hace un sleep para confirmar que es el CLK quien ejecuta el wait el
    primero. Antes que el Timer pueda enviar la señal.

--- src/erlojua/erlojua.c
+++ src/erlojua/erlojua.c
@@ -19,19 +19,19 @@ int main()
     
     pthread_t tr_clk, tr_tmr1, tr_tmr2;
 
-    int timer_ticks = 3000;
+    int timer_ticks = 10;
     int timer_kop = TIMER_KOP;
-    printf("Clock started1\n");
+    printf("Clock execute\n");
     if (pthread_create(&tr_clk, NULL, clock_start, (void *) &timer_kop)){
         printf("Error: Clock started\n");
     }
     sleep(5);
-    printf("Timer started1\n");
+    printf("Timer execute\n");
     if (pthread_create(&tr_tmr1, NULL, timer_start, (void *) &timer_ticks)){
-        printf("Error: Timer started"); 
+        printf("Error: Timer started\n"); 
     }
     
-    pthread_join(tr_clk, NULL);
+-    pthread_join(tr_clk, NULL);
     //pthread_join(tr_tmr1, NULL); 
     
     exit(0);
@@ -39,38 +39,58 @@ int main()
 
 
 void *clock_start(void *vargp){
-    printf("Hey im ck2");
+    printf("Hey im clk\n");
     int *p_timer_kop = (int *)vargp;
     int done;
+    printf("CLK: Im going to lock MUTEX\n");
+    pthread_mutex_lock(&mutex_clk); // Mutexa blokeatu
+    printf("CLK: MUTEX LOCKED for me\n");
     while(1){
+        sleep(1);
         done = 0;
         // printf("Whilera sartu da clock");
-        pthread_mutex_lock(&mutex_clk); // Mutexa blokeatu
+        
         while ( done < *p_timer_kop )
         {
+            printf("CLK: Im releasing MUTEX (UNLOCK)\n");
+            printf("CLK: Im waiting -> cond_tmr_arreta\n");
             pthread_cond_wait(&cond_tmr_arreta, &mutex_clk );
+            printf("CLK: End Wait --> cond_tmr_arreta recived\n");
+            printf("CLK: Im locked\n");
             done++;
         }
+        printf("CLK: Sending broadcast -> cond_clk_ziklo\n");
         pthread_cond_broadcast(&cond_clk_ziklo);
-        pthread_mutex_unlock(&mutex_clk);
+        printf("CLK: Broadcast sent -> cond_clk_ziklo\n");
+        // pthread_mutex_unlock(&mutex_clk);
     }
 }
 
 void *timer_start(void *argv){
-    // printf("Timer started2");
+    printf("Hey im timer\n");
     int *count = (int *) argv;
+
+    printf("TIMER: Im going to lock\n");
     pthread_mutex_lock(&mutex_clk); // zer gertatuko litzateke, lerro hau clock-aren mutex-lock-a baino lehen exekutatuko balitz?
+    printf("TIMER: Im locked\n");
     // printf("Whilera sartu timer");
     
     while(1){
+        sleep(1);
         count--;
         if (count < 0){
             timer_send_signal();
             count=(int *) argv;
         }
-
+        printf("TIMER: Sending signal -> cond_tmr_arreta\n");
         pthread_cond_signal(&cond_tmr_arreta);
+        printf("TIMER: Signal sent -> cond_tmr_arreta\n");
+
+        printf("TIMER: Im UNlocked\n");
+        printf("TIMER: Im waiting -> cond_clk_ziklo\n");
         pthread_cond_wait(&cond_clk_ziklo, &mutex_clk);
+        printf("TIMER: End Wait --> cond_clk_ziklo recived\n");
+        printf("TIMER: Im locked\n");
     }
 }
 
