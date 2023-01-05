60d338979952fa0f5a83ac39ce54b20381886f38
Mikel Laorden <laordenmikel@gmail.com>
2023-01-05 12:04:34 +0100

    Erlojua refactor + functionlities ++
    # Functionalities:
    - init structs: Clock + Timer
    - stop timer
    - change timer freq

--- include/data_structures/clk_s.h
+++ include/data_structures/clk_s.h
@@ -2,23 +2,44 @@
 #define CLOCK_H
 
 # include <pthread.h>
+
+#define CLK_STATUS_RUNNING 1
+#define CLK_STATUS_TERMINATE 2
+
+
 typedef void (*DoActionWhenTick)();
-struct timer_s{
-    struct clk_mutex_s * linked_clk;
+struct clk_timer_s{
+    struct clk_clock_s * linked_clk;
     int tmr_tick_freq;
     DoActionWhenTick TickAction;
     void * tickActionParams;
+    int tmr_status;
+    int tick_count;
 };
+typedef struct clk_timer_s clk_timer_s;
 
-struct clk_mutex_s{
+struct clk_clock_s{
     pthread_mutex_t * mutex_clk;
     pthread_cond_t * cond_clk_ziklo;
     pthread_cond_t * cond_tmr_arreta;
     int tmr_kop_tick_consumed;
     int tmr_total_kop;
+    int clk_status;
 };
+typedef struct clk_clock_s clk_clock_s;
 
-void *timer_start(void *tmr_struct);
-void *clock_start(void *clk_struct);
+void *clk_timer_start(void *tmr_struct);
+void *clk_clock_start(void *clk_struct);
+
+void clk_clock_init(clk_clock_s *clk_struct);
+
+void clk_timer_init(clk_timer_s *tmr_struct, clk_clock_s *clk_struct, 
+    int tmr_tick_freq, DoActionWhenTick tickaction, void * tickActionParams); // TODO:
+
+void clk_stop_timer(clk_timer_s *tmr_struct); // TODO:
+
+
+void clk_change_timer_tickFreq(clk_timer_s * tmr_struct, int new_tickFreq, int restart); // TODO:
+
 
 #endif
--- src/erlojua/erlojua.c
+++ src/erlojua/erlojua.c
@@ -6,40 +6,92 @@
 #include <lnklist_s.h>
 #include <clk_s.h>
 
-void *clock_start(void *clk_mutex_s){
-    struct clk_mutex_s * clk = (struct clk_mutex_s *)clk_mutex_s;
+void *clk_clock_start(void *clk_mutex_s){
+    clk_clock_s * clk;
+    clk = (clk_clock_s *)clk_mutex_s;
 
     pthread_mutex_lock(clk->mutex_clk); // Mutexa blokeatu
     while(1){
-        clk->tmr_kop_tick_consumed = 0;   
-        printf("CLK: tick\n");
-        while ( clk->tmr_kop_tick_consumed < clk->tmr_total_kop)
+        printf("\n\n+ CLK: tick %d - %d \n", clk->tmr_kop_tick_consumed, clk->tmr_total_kop);
+        while (clk->tmr_kop_tick_consumed < clk->tmr_total_kop || 
+           clk->tmr_total_kop == 0)
         {
             pthread_cond_wait(clk->cond_tmr_arreta, clk->mutex_clk );
         }
         pthread_cond_broadcast(clk->cond_clk_ziklo);
+        clk->tmr_kop_tick_consumed = 0;
+        sleep(1);
     }
+    pthread_mutex_unlock(clk->mutex_clk);
 }
 
-void *timer_start(void *p_timer_s){
-    struct timer_s * tmr = (struct timer_s *) p_timer_s;
-
-    struct clk_mutex_s * t_clk = tmr->linked_clk;
-
-    int count = tmr->tmr_tick_freq;
+void *clk_timer_start(void *p_timer_s){
+    clk_timer_s * tmr;
+    clk_clock_s * t_clk;
+    
+    tmr = (clk_timer_s *) p_timer_s;
+    t_clk = tmr->linked_clk;
+    tmr->tick_count = tmr->tmr_tick_freq;
 
     pthread_mutex_lock(t_clk->mutex_clk);
-    
-    while(1){
-        count -= 1;
-        printf("TMR_f%d: count %d \n",tmr->tmr_tick_freq, count);
+    t_clk->tmr_total_kop++;
+
+    while(tmr->tmr_status != CLK_STATUS_TERMINATE){
+        tmr->tick_count -= 1;
+        printf("TMR_f%d: count %d \n",tmr->tmr_tick_freq, tmr->tick_count);
         
-        if (count <= 0){
+        if (tmr->tick_count <= 0){
             tmr->TickAction(tmr->tickActionParams);
-            count = tmr->tmr_tick_freq;
+            tmr->tick_count = tmr->tmr_tick_freq;
         }
         t_clk->tmr_kop_tick_consumed++;
         pthread_cond_signal(t_clk->cond_tmr_arreta);
         pthread_cond_wait(t_clk->cond_clk_ziklo, t_clk->mutex_clk);
     }
+
+    t_clk->tmr_total_kop--;
+    pthread_mutex_unlock(t_clk->mutex_clk);
+}
+
+void clk_clock_init(clk_clock_s * clk_struct){
+    pthread_mutex_t * g_mutex_clk = (pthread_mutex_t *) malloc(sizeof(pthread_mutex_t));
+    pthread_cond_t * g_cond_clk_ziklo = (pthread_cond_t *) malloc(sizeof(pthread_cond_t));
+    pthread_cond_t * g_cond_tmr_arreta = (pthread_cond_t *) malloc(sizeof(pthread_cond_t));
+
+    pthread_mutex_init(g_mutex_clk, NULL);
+    pthread_cond_init(g_cond_clk_ziklo, NULL);
+    pthread_cond_init(g_cond_tmr_arreta, NULL);
+    
+    clk_struct->mutex_clk = g_mutex_clk;
+    clk_struct->cond_clk_ziklo = g_cond_clk_ziklo;
+    clk_struct->cond_tmr_arreta = g_cond_tmr_arreta;
+    clk_struct->tmr_kop_tick_consumed = 0;
+    clk_struct->tmr_total_kop = 0;
+    clk_struct->clk_status = CLK_STATUS_RUNNING;
+}
+
+void clk_timer_init(clk_timer_s *tmr_struct, clk_clock_s *clk_struct, int tmr_tick_freq, DoActionWhenTick tickaction, void * tickActionParams){
+    tmr_struct->linked_clk = clk_struct;
+    tmr_struct->tmr_tick_freq = tmr_tick_freq;
+    tmr_struct->TickAction = tickaction;
+    tmr_struct->tickActionParams = tickActionParams;
+    tmr_struct->tmr_status = CLK_STATUS_RUNNING;
+    tmr_struct->tick_count = 0;
+}
+
+void clk_stop_timer(clk_timer_s *tmr_struct){
+    tmr_struct->tmr_status = CLK_STATUS_TERMINATE;
+}
+
+void clk_change_timer_tickFreq(clk_timer_s * tmr_struct, int new_tickFreq, int restart){
+    if(restart){
+        tmr_struct->tick_count = new_tickFreq;
+    }else{
+        int consumed_ticks = tmr_struct->tmr_tick_freq - tmr_struct->tick_count;
+        tmr_struct->tick_count = new_tickFreq - consumed_ticks;
+        if(tmr_struct->tick_count < 1){
+            tmr_struct->tick_count = 1;
+        }
+    }
+    tmr_struct->tmr_tick_freq = new_tickFreq;
 }
